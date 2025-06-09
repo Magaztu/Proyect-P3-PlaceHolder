31fb4a19d34b374c00e74d0bf4b0707e65cdd4fd
Mathias Toala <fetoala@outlook.com>
2025-06-08 23:51:16 -0500

    Added a bunch of dummy data (mostly useless)

---
+++ env/datos.js
@@ -0,0 +1,28 @@
+export const clientes = [
+    {nombre: "Rodrigo", apellido: "Espinoza"},
+    {nombre: "María", apellido: "Valdez"},
+    {nombre: "Juan", apellido: "Marcet"}]
+
+export const peliculas = [
+    {titulo: "Karate Kid", duracion: "120 minutos", categoria: "Acción", descripcion: "Un niño aprende Karate"},
+    {titulo: "Kung-Fu Panda", duracion: "90 minutos", categoria: "Acción", descripcion: "Un panda aprende Kung-Fu"},
+    {titulo: "Destino Final", duracion: "120 minutos", categoria: "Acción", descripcion: "Las garras del destino te atraparán"},
+    {titulo: "Titanic", duracion: "180 minutos", categoria: "Romance", descripcion: "El amor no conoce clases sociales, ni sacrificios"},
+    {titulo: "Ciudades de Papel", duracion: "120 minutos", categoria: "Romance", descripcion: "El amor no conoce la distancia, ni acertijos"},
+    {titulo: "After", duracion: "90 minutos", categoria: "Romance", descripcion: "Es mejor no saber... creo..."},
+    {titulo: "Supercool", duracion: "90 minutos", categoria: "Comedia", descripcion: "Un grupo de amigos planifican una fiesta inolvidable"},
+    {titulo: "Los Amigos del Barrio", duracion: "80 minutos", categoria: "Comedia", descripcion: "Barrios, bandas y armas. ¿Qué puede salir mal?"},
+    {titulo: "Pixeles", duracion: "120 minutos", categoria: "Comedia", descripcion: "Un chavo tiene una aventura bien chavorrusca"}
+]
+// export const peliculas = [
+//     {categoria: "Acción", peliculas:["Karate kid","Kung-Fu Panda","Destino Final"]},
+//     {categoria: "Romance", peliculas:["Titanic","Ciudades de Papel","After"]},
+//     {categoria: "Comedia", peliculas:["Supercool","Los Amigos del Barrio","Pixeles"]}
+// ]
+// export const alquileres = [
+//     {},
+//     {},
+//     {}
+// ]
+
+//Ya no me dieron ganas de seguir escribiendo datos, igual no los vamos a usar dnqwjkdnq, voy a simular un input cualquiera.
